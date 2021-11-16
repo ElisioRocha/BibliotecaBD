f198ffbb2b42153d3c9978a96a21b6a8eeed3549
Elísio Rocha <Elsio Rocha@ELiRock_pc>
2021-11-16 11:18:40 +0000

    Teste de comit

--- src/pt/rochalivrosbd/livros/BandaDesenhada.java
+++ src/pt/rochalivrosbd/livros/BandaDesenhada.java
@@ -1,16 +1,17 @@
-
 package pt.rochalivrosbd.livros;
 
+import pt.rochalivrosbd.pessoas.Autor;
 
 public class BandaDesenhada {
      
-    /*
-        autor
-        númeroDePáginas
-        peso
-        cores
-        tamanho
-        dataPublicação
-    */
+    /**/
+        Autor autor; //campo do tipo referencia
+        int númeroDePáginas;//p.ex. 25, 95, 125
+        double peso; //em quilos, p.ex. 0.125kg, 0.95kg
+        boolean cores;
+        String tamanho;
+        int anoPubicação; //p.ex: 1995, 2001, 1954
+        String titulo; //campo do tipo referencia
+    /**/    
     
 }
---
+++ src/pt/rochalivrosbd/pessoas/Autor.java
@@ -0,0 +1,12 @@
+package pt.rochalivrosbd.pessoas;
+
+import java.util.Date;
+
+public class Autor {
+    
+    String nomePróprio;
+    String apelido;
+    Date dataDeNascimento;
+    String nacionalidade; //p.ex. português, belga, etc.
+    
+}
