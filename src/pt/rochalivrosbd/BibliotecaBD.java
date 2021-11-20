893911fc00d8468bfecf4cf55df22ce6f1073727
Elísio Rocha <Elsio Rocha@ELiRock_pc>
2021-11-20 00:27:27 +0000

    Com .stream .filter e .foreach a funcionar nas pesquisas e agrupar BD's

--- src/pt/rochalivrosbd/BibliotecaBD.java
+++ src/pt/rochalivrosbd/BibliotecaBD.java
@@ -1,7 +1,9 @@
 package pt.rochalivrosbd;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import pt.rochalivrosbd.gerirdados.BaseDadosBD;
 import pt.rochalivrosbd.livros.BandaDesenhada;
 
@@ -10,23 +12,46 @@ public class BibliotecaBD {
     public static void main(String[] args) {
 
         System.out.println("Bem-vinda|o à biblioteca de livros de banda desenhada.");
-        BaseDadosBD.setBaseDadosBD();
+        ArrayList<BandaDesenhada> biblioteca = BaseDadosBD.setBaseDadosBD();
 
         Scanner entradaPedido = new Scanner(System.in);
         System.out.println("Que personagem de Banda Desenhada pretende pesquisar? ");
         String personagemPedida = entradaPedido.nextLine();
 
         ArrayList<BandaDesenhada> pedidoBandaDesenhada = new ArrayList();
-        pedidoBandaDesenhada = devolveBandasDesenhadas(personagemPedida);
+        //pedidoBandaDesenhada = devolveBandasDesenhadas(personagemPedida,biblioteca);
 
+        /*##Aqui temos um forEach que percorre numa única linha##*/
+        //biblioteca.forEach(p -> System.out.println(p.getTítulo()));
+        /*##Aqui temos um exemplo de utilização de lambdaExpression com um consumer*/
+        //Consumer lambdaExpression = p -> System.out.println(p);
+        //biblioteca.forEach(lambdaExpression);
+        if(biblioteca.isEmpty() == false){
+            
+                biblioteca.stream()
+                          .filter(s -> s.getPersonagem().equalsIgnoreCase(personagemPedida))
+                          .forEach(p -> pedidoBandaDesenhada.add(p));
+                
+            if (pedidoBandaDesenhada.isEmpty() == false) {
+                pedidoBandaDesenhada.forEach(p -> System.out.println("Banda Desenhada de: "
+                                           + p.getPersonagem() + " '" + p.getTítulo() + "'"));
+            }
+            else {
+            System.out.println("Banda Desenhada de: '" + personagemPedida + "' não existe nesta biblioteca!");
+            }
+        }
+        else{
+            System.out.println("De momento a Biblioteca de Bandas Desenhadas está vazia!");
+        }
+        
         System.out.println("Agradecemos o seu pedido de banda desenhada.");
 
     }
 
-    public static ArrayList devolveBandasDesenhadas(String personagem) {
+    public static ArrayList devolveBandasDesenhadas(String personagem, ArrayList biblioteca) {
 
         ArrayList<BandaDesenhada> baseDadosBD, respostaBD = new ArrayList();
-        baseDadosBD = BaseDadosBD.getBaseDadosBD();
+        baseDadosBD = biblioteca;
 
         if (baseDadosBD != null) {
 
@@ -38,12 +63,12 @@ public class BibliotecaBD {
                 }
             }
             if (respostaBD.isEmpty() == true) {
-                System.out.println("Banda Desenhada de: " + personagem + " não existe nesta biblioteca!");
+                System.out.println("Banda Desenhada de: '" + personagem + "' não existe nesta biblioteca!");
             }
-        }
-        else
+        } else {
             System.out.println("De momento a Biblioteca de Bandas Desenhadas está vazia!");
-        
+        }
+
         return respostaBD;
     }
 
--- src/pt/rochalivrosbd/gerirdados/BaseDadosBD.java
+++ src/pt/rochalivrosbd/gerirdados/BaseDadosBD.java
@@ -6,28 +6,29 @@ import pt.rochalivrosbd.livros.BandaDesenhada;
 import pt.rochalivrosbd.pessoas.Autor;
 
 public class BaseDadosBD {
-    private static ArrayList <Autor> coleçãoAutores;// = new ArrayList();// hergé, morris, goscinny;
-    private static ArrayList <BandaDesenhada> coleçãoBD;// = new ArrayList();//osDaltons, unicorno, oFoguetão, osRomanos;
+ // private static ArrayList <Autor> coleçãoAutores = new ArrayList();// hergé, morris, goscinny;
+ // private static ArrayList <BandaDesenhada> coleçãoBD = new ArrayList();//osDaltons, unicorno, oFoguetão, osRomanos;
 
     BaseDadosBD() {
     }
     
-    public static void setBaseDadosBD() {
-        coleçãoAutores = new ArrayList();
+    public static ArrayList setBaseDadosBD() {
+        ArrayList <Autor> coleçãoAutores = new ArrayList();
         coleçãoAutores.add(new Autor("George", "Prosper Remi", "Hergé", LocalDate.of(1907, 5, 22)));
         coleçãoAutores.add(new Autor("Maurice", "de Bévère", "Morris", LocalDate.of(1923,12,1)));
         coleçãoAutores.add(new Autor("René", "Goscinny", "René Goscinny",LocalDate.of(1926, 8, 14)));
         
-        coleçãoBD = new ArrayList();
+        ArrayList <BandaDesenhada> coleçãoBD = new ArrayList();
         coleçãoBD.add(new BandaDesenhada(coleçãoAutores.get(1), true, 1996, "Os Dalton","Lucky Luke"));
         coleçãoBD.add(new BandaDesenhada(coleçãoAutores.get(0), true, 1985, "O unicorno","Tintim"));
         coleçãoBD.add(new BandaDesenhada(coleçãoAutores.get(0), true, 1991, "Missão à Lua","Tintim"));
         coleçãoBD.add(new BandaDesenhada(coleçãoAutores.get(2), true, 1978, "Asterix e Obelix contra César","Asterix"));
         
+        return coleçãoBD;
     }
-    
+    /* Inicialmente guardava a coleção de Bandas Desenhadas num Field private desta classe
     public static ArrayList getBaseDadosBD() {
         return coleçãoBD;                
     }    
-    
+    */
 }
